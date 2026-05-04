dc3f7346cfee64291423fc7b0b2f5e4e3b22a73c
Eugenio Perez <84157407+Yug1900@users.noreply.github.com>
2026-05-04 18:51:56 -0400

    Made build.sh cleanup after having generated iso and having placed it ./build parent directory

--- build.sh
+++ build.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 set -e
 cd ./build
-sudo lb clean --purge
+lb clean --purge || sudo lb clean --purge
 lb config \
   --distribution bookworm \
   --architectures amd64 \
-  --debian-installer false \
+  --debian-installer none \
   --archive-areas "main contrib non-free non-free-firmware" \
   --binary-images iso-hybrid \
   --bootappend-live "boot=live components quiet splash persistence" \
   --apt-recommends true \
   --compression xz
 sudo lb build
+mv *.iso ../
+lb clean --purge || sudo lb clean --purge
