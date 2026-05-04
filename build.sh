63de7b33cd4c83952bd7ded1cee345c74b560bf4
Eugenio Perez <84157407+Yug1900@users.noreply.github.com>
2026-05-04 18:26:55 -0400

    Fixed config not applied on build

--- build.sh
+++ build.sh
@@ -2,4 +2,13 @@
 set -e
 cd ./build
 sudo lb clean --purge
+lb config \
+  --distribution bookworm \
+  --architectures amd64 \
+  --debian-installer false \
+  --archive-areas "main contrib non-free non-free-firmware" \
+  --binary-images iso-hybrid \
+  --bootappend-live "boot=live components quiet splash persistence" \
+  --apt-recommends true \
+  --compression xz
 sudo lb build
